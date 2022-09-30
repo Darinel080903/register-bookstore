f38ba39b20afbfe95fe2d8c152df7f986d78e8e9
Darinel080903 <darinelmr12345@gmail.com>
2022-09-29 22:29:34 -0500

    Actualizacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import '../css/Header.css'
+import Imagen from '../img/imagen-02.png'
 
 function Header(props) {
     return(
         <div className='Navbar'>
             <div className="blue-bg">
                 <img className="imagen-01 alinear-izquierda" 
-                src={(`../src/img/imagen-${props.imagen}.png`)}/>
+                src={Imagen}/>
                 <h1>REGISTER OF BOOK STORE</h1>
             </div>
         </div>
